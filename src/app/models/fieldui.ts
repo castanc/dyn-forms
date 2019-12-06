06e280289daf51c96ad5e92e993edf6604ab3491
castanc <54039892+castanc@users.noreply.github.com>
2019-12-06 16:44:07 -0300

    Create fieldui.ts

---
+++ src/app/models/fieldui.ts
@@ -0,0 +1,13 @@
+export class FieldUI{
+    IdField: number;
+    Name: string;
+    Label: string;
+    HtmlType: string;   //input,textarea,select,checkbox,radio button
+    HtmlInputType: string; //text,number,email,url,date
+    HtmlSelectId: string;
+    PlaceHolder: string;
+    LabelMode: string;  //floating, left, placeholder 
+    MaxLength: number;
+    Mask: string;
+    ImageId: number;    
+}
