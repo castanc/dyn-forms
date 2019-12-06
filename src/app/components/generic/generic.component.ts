2e1d0a7f35a2898d99f16622e1cb37794d8eb713
castanc <54039892+castanc@users.noreply.github.com>
2019-12-06 16:42:02 -0300

    Update generic.component.ts

--- src/app/components/generic/generic.component.ts
+++ src/app/components/generic/generic.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
+
+import { BackEndService } from '../../services/backend'
+
 
 @Component({
-  selector: 'app-generic',
+  selector: 'app-generic-component',
   templateUrl: './generic.component.html',
   styleUrls: ['./generic.component.css']
 })
 export class GenericComponent implements OnInit {
 
-  constructor() { }
+  
+  constructor( protected bs: BackEndService,
+    protected router: Router,
+    protected route: ActivatedRoute)
+  {
+    console.log("genericCOmponent.constructor())");
+  }
+
 
   ngOnInit() {
   }
