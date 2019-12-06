25dfeaf622f0c8f93d0bf120e7e2e09b8663dfef
castanc <54039892+castanc@users.noreply.github.com>
2019-12-06 16:46:13 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { APP_BASE_HREF, LocationStrategy, HashLocationStrategy } from '@angular/common';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainMenuComponent } from './components/main-menu/main-menu.component';
 import { GenericComponent } from './components/generic/generic.component';
+import { BackEndService } from './services/backend';
+import { BaseComponent } from './components/base/base.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainMenuComponent,
-    GenericComponent
+    GenericComponent,
+    BaseComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [BackEndService,
+    {provide: APP_BASE_HREF, useValue: '/'},
+    {provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
