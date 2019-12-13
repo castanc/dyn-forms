85bf0fc920bbd3167da9cbc5f79652bbd443a21d
castanc <54039892+castanc@users.noreply.github.com>
2019-12-13 16:53:50 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { GenericComponent} from './components/generic/generic.component'
-import { BaseComponent } from './components/base/base.component'
 
 
 const routes: Routes = [];
-const appRoutes: Routes = [
-  {path: '', redirectTo: '/Nuevo', pathMatch: 'full'},
-  {path: 'create', component: GenericComponent },
-];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
