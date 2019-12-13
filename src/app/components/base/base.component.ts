d47fc0d04c9db5a37687cf438e5e65d547a7debc
castanc <54039892+castanc@users.noreply.github.com>
2019-12-13 16:47:00 -0300

    Create base.component.ts

---
+++ src/app/components/base/base.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormsModule, NgForm  } from '@angular/forms';
+
+import { BackEndService } from '../../services/backend'
+
+@Component({
+  selector: 'app-base',
+  templateUrl: './base.component.html',
+  styleUrls: ['./base.component.css']
+})
+export class BaseComponent implements OnInit {
+@ViewChild('cDate', {static:false}) cDate: ElementRef 
+@ViewChild('cTime', {static:false}) cTime: ElementRef 
+@ViewChild('f2', {static:false}) form: ElementRef 
+
+
+  constructor( protected bs: BackEndService,
+    protected router: Router,
+    protected route: ActivatedRoute)
+  {
+    console.log("baseComponent.constructor())");
+  }
+
+
+  ngOnInit() {
+  }
+
+  onSelectedItemChanged(formName: string)
+  {
+    this.bs.LoadForm();
+    if (this.bs.Form )
+    {
+      this.bs.SetUserDate(this.cDate.nativeElement.value,this.cTime.nativeElement.value);
+      this.router.navigate(['create']);
+    }
+    else
+    {
+        throw new Error("Invalid Form")
+    }
+
+  }
+
+  onSubmit(form: NgForm){
+  }
+
+
+}
