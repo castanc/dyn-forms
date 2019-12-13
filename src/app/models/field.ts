d417dab217cd6f045d2c6e6a4166c8e135d40e85
castanc <54039892+castanc@users.noreply.github.com>
2019-12-13 16:51:56 -0300

    Update field.ts

--- src/app/models/field.ts
+++ src/app/models/field.ts
@@ -2,7 +2,16 @@ export class Field<T>{
     Id: number;
     Name: string;
     Value: T;
-    Required : boolean;
+    Required : boolean = false;
     Max: T;
     Min: T;
+
+    constructor(name:string,required:boolean = false,value: T=null,max: T = null, min: T = null,id:number=0){
+        this.Id = id;
+        this.Name = name;
+        this.Required = required;
+        this.Min = min;
+        this.Max = max;
+        this.Value = value;
+    }
 }
