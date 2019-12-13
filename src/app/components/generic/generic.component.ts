5d521409a6287db73f5115c2c3bfc8b478c0ec12
castanc <54039892+castanc@users.noreply.github.com>
2019-12-13 16:49:09 -0300

    Update generic.component.ts

--- src/app/components/generic/generic.component.ts
+++ src/app/components/generic/generic.component.ts
@@ -1,27 +1,54 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
 import { FormsModule, NgForm } from '@angular/forms';
 
+import { Field } from '../../models/field'
+import { FieldUI } from '../../models/fieldui'
+import { Form } from '../../models/form'
+import { ListItem } from '../../models/list-item'
+import { ComboItem } from '../../models/combo-item'
 import { BackEndService } from '../../services/backend'
-
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-generic-component',
+  selector: 'app-generic',
   templateUrl: './generic.component.html',
   styleUrls: ['./generic.component.css']
 })
 export class GenericComponent implements OnInit {
+  f: Form;
 
-  
-  constructor( protected bs: BackEndService,
-    protected router: Router,
-    protected route: ActivatedRoute)
-  {
-    console.log("genericCOmponent.constructor())");
-  }
+  constructor(private bs: BackEndService, router: Router) {
+    if ( bs.Form)
+    {
+      this.f = bs.Form;
+      console.log("form:",this.f);
+    }
+    else
+      router.navigate(['']);
 
+   }
+
+   private getField(id:number):Field<any>
+   {
+     return this.bs.Form.GetField(id);
+   }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm){
+
+    //todo: interesting maps check later
+    /*
+    let m  = new Map();
+    for (var index = 0; index < this.f.Fields.length; index++)
+    {
+      m.set(this.f.Fields[index].Name, this.f.Fields[index].Value);
+    }
+    console.log(m);
+    */
+    this.f.UpdateDTO();
+
   }
 
 }
