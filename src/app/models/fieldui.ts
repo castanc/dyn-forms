3431458620c1a55b86a7dadf24b6d333df9d48d7
castanc <54039892+castanc@users.noreply.github.com>
2019-12-13 16:52:29 -0300

    Update fieldui.ts

--- src/app/models/fieldui.ts
+++ src/app/models/fieldui.ts
@@ -1,5 +1,8 @@
+import { ListItem } from '../models/list-item'
+
 export class FieldUI{
     IdField: number;
+    Id: number;
     Name: string;
     Label: string;
     HtmlType: string;   //input,textarea,select,checkbox,radio button
@@ -10,4 +13,35 @@ export class FieldUI{
     MaxLength: number;
     Mask: string;
     ImageId: number;    
+    List: Array<ListItem> = null;
+    SelectedValue: number = 0;
+
+    constructor(idField:number,
+        name:string,
+        htmlType:string="input",
+        htmlInputType:string="text",
+        maxLength:number = 32,
+        label:string=name,
+        placeholder:string=label,
+        htmlSelectId:string = "",
+        labelMode:string="top",
+        )
+    {
+        this.IdField = idField;
+        this.Name = name;
+        this.HtmlType = htmlType;
+        this.HtmlInputType = htmlInputType;
+        this.HtmlSelectId = htmlSelectId;
+        this.IdField = idField;
+        this.Label = label;
+        this.PlaceHolder = placeholder;
+        this.LabelMode = labelMode;
+    }
+
+    AddListItem(item: ListItem):void
+    {
+        if ( this.List == null )
+            this.List = new Array<ListItem>();
+        this.List.push(item);
+    }
 }
