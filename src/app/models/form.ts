ee94dd9579f61ce3321c6347e0a6c7f51ac08420
castanc <54039892+castanc@users.noreply.github.com>
2019-12-16 18:02:56 -0300

    Update form.ts

--- src/app/models/form.ts
+++ src/app/models/form.ts
@@ -1,75 +1,10 @@
-import { Field } from './field'
-import { FieldUI } from './fieldUI'
-import { ComboItem } from './combo-item'
-import { ListItem } from './list-item'
-import { DTORecord } from './DTORecord'
-import { DTOField } from './DTOField'
-import { TypeofExpr } from '@angular/compiler'
-import { RelatedMap } from './related-map'
+import {DTOField } from '../models/DTOField'
+import { Form } from '../models/form'
 
-export class Form{
-    Id: number = 0;
-    RecordType: number = 0;
-    Name = "";
-    Fecha: Date;
-    IsTabular: boolean = false;
-    Title: string = "";
-    private fields: Array<Field<any>>;
-    private dtoFields: Array<DTOField<any>> = [];
-    Rows: Array<Array<DTOField<any>>> = [];
-    private ui: Array<FieldUI>;
-    CurrentRow: number = 0;
-    ChildForm: string;
-    IgnoreFields = false;
-    Infrastructure = false;
-    RelatedFormName: string;
+export interface IPostInput {
 
-    get Fields(){ return this.fields;}
-    get UI() { return this.ui;}
-    get DTOFields() { return this.Rows[this.CurrentRow];}
-
-    public Maps: Array<RelatedMap> = [];
+    //PostInput(fieldName: string,value: any, fieldsMap: Map<string,number>, dtoFields: Array<DTOField<any>>)
+    PostInput(fieldName: string,value: any, form: Form)
 
 
-    constructor(){
-        this.fields = new Array<Field<any>>();
-        this.ui = new Array<FieldUI>();
-    }
-
-    AddFirstRow(){
-        this.Rows.push(this.dtoFields);
-    }
-
-    GetField(id:number):Field<any>{
-        return this.fields.filter(x => x.Id == id)[0];
-    }
-
-    AddFieldTyped<T>(f: Field<T>){
-        f.Id = this.Fields.length;
-        this.Fields.push(f);
-
-        let dtoField = new DTOField<T>();
-        dtoField.Value = f.Value;
-        this.dtoFields.push(dtoField);
-        
-    }
-   
-    AddFieldUI(f: FieldUI){
-        f.Id = this.UI.length;
-        this.UI.push(f);
-
-    }
-
-    UpdateDTO( )
-    {
-        this.fields[1].Value = this.Fecha;
-        this.fields[2].Value = this.RecordType;
-        for (let index = 0; index < this.fields.length; index++)
-        {
-          console.log("Field type:",this.fields[index].Name, this.fields[index].Value, typeof(this.fields[index].Value));
-        }
-    
-    }
-
-  
 }
