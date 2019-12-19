5983a3117298d0185fd5a041281e2461c58e0b79
castanc <54039892+castanc@users.noreply.github.com>
2019-12-19 17:02:56 -0300

    Update food-cantidad.ts

--- src/app/hooks/food-cantidad.ts
+++ src/app/hooks/food-cantidad.ts
@@ -1,16 +1,19 @@
-import { IPostInput } from './post-input-interface'
+import { IHook } from './hook-interface'
 import { DTOField } from '../models/DTOField'
 import { Form } from '../models/form'
 
-export class FoodCantidad implements IPostInput {
+export class FoodCantidad implements IHook {
     
-    PostInput(fieldName: string,value: any,  f: Form)
+    Form: Form;
+    Process(uiFieldId:number)
     {
+        /*
         f.DTOFields[f.FieldsMap.get("Calories")].Value = f.DTOFields[f.FieldsMap.get("Calories")].Value * f.DTOFields[f.FieldsMap.get("Cantidad")].Value / value ;
         f.DTOFields[f.FieldsMap.get("Fat")].Value = f.DTOFields[f.FieldsMap.get("Fat")].Value * f.DTOFields[f.FieldsMap.get("Cantidad")].Value / value ;
         f.DTOFields[f.FieldsMap.get("Sugar")].Value = f.DTOFields[f.FieldsMap.get("Sugar")].Value * f.DTOFields[f.FieldsMap.get("Cantidad")].Value / value ;
         f.DTOFields[f.FieldsMap.get("Sodium")].Value = f.DTOFields[f.FieldsMap.get("Sodium")].Value * f.DTOFields[f.FieldsMap.get("Cantidad")].Value / value ;
         return f.DTOFields;
+        */
     }
 
 }
