930bb155b12f4b0f48d0ae5a69a669be36cf3346
castanc <54039892+castanc@users.noreply.github.com>
2019-12-19 17:03:15 -0300

    Update hook-factory.ts

--- src/app/hooks/hook-factory.ts
+++ src/app/hooks/hook-factory.ts
@@ -1,15 +1,18 @@
-import { IPostInput } from './post-input-interface'
+import { IHook } from './hook-interface'
 import { FoodCantidad } from './food-cantidad';
-import { FoodItemUrl } from './fooditem-url';
+import { SaveImage } from './save-image';
+import { RefreshImage } from './refresh-image'
 
 export class HookFactory{
 
-    GetHook(key:string):IPostInput{
+    GetHook(key:string):IHook{
         key = key.toLowerCase();
         if ( key == "food-cantidad")
             return new FoodCantidad();
-        else if (key == "fooditem-url")
-            return new FoodItemUrl();
+        else if (key == "save-image")
+            return new SaveImage();
+        else if ( key == "refresh-image")
+            return new RefreshImage();
         return null;
     }
 }
